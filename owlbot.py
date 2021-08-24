f16640ed8814e8cc208645bfde1d3046715a7781
Dan Lee <dandhlee@google.com>
2021-08-24 02:29:21 +0000

    docs: migrate to main branch

--- owlbot.py
+++ owlbot.py
@@ -31,3 +31,38 @@ s.move(templated_files, excludes=["docs/multiprocessing.rst"])
 python.py_samples(skip_readmes=True)
 
 s.shell.run(["nox", "-s", "blacken"], hide_output=False)
+
+# ----------------------------------------------------------------------------
+# Main Branch migration
+# ----------------------------------------------------------------------------
+
+s.replace(
+  "*.rst",
+  "master",
+  "main"
+)
+
+s.replace(
+  "CONTRIBUTING.rst",
+  "kubernetes/community/blob/main",
+  "kubernetes/community/blob/master"
+)
+
+s.replace(
+  "docs/conf.py",
+  "master",
+  "main"
+)
+
+s.replace(
+  "docs/conf.py",
+  "main_doc",
+  "root_doc"
+)
+
+s.replace(
+  ".kokoro/*",
+  "master",
+  "main"
+)
+
