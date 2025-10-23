d020cff8f9e040363c7d7534afd27b4ec3a9a796
Chalmer Lowe <chalmerlowe@google.com>
2025-10-23 10:04:50 -0400

    Apply suggestion from @chalmerlowe

--- owlbot.py
+++ owlbot.py
@@ -60,7 +60,7 @@ s.replace(
 
 s.replace(
     "noxfile.py",
-    r'SYSTEM_TEST_PYTHON_VERSIONS: List[str] = ["3.8"]',
+    r'SYSTEM_TEST_PYTHON_VERSIONS: List\[str\] = \["3.8"\]',
     'SYSTEM_TEST_PYTHON_VERSIONS: List[str] = ["3.10", "3.11", "3.12", "3.13"]'
 )
 
